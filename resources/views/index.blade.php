ca127c890d4fdaa8d2715972552cc83c3a6511ad
Seymur Omarov <omarov.seymur@outlook.com>
2018-07-20 14:19:54 +0400

    update google optimize

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,6 +25,24 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
           integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <style>.async-hide {
+            opacity: 0 !important
+        } </style>
+    <script>(function (a, s, y, n, c, h, i, d, e) {
+            s.className += ' ' + y;
+            h.start = 1 * new Date;
+            h.end = i = function () {
+                s.className = s.className.replace(RegExp(' ?' + y), '')
+            };
+            (a[n] = a[n] || []).hide = h;
+            setTimeout(function () {
+                i();
+                h.end = null
+            }, c);
+            h.timeout = c;
+        })(window, document.documentElement, 'async-hide', 'dataLayer', 4000,
+            {'GTM-T8QFP89': true});</script>
+
     <script>
         (function (i, s, o, g, r, a, m) {
             i['GoogleAnalyticsObject'] = r;
