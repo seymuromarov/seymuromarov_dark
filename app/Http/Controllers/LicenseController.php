5f30061d0f1b633cfef0a2262111f2af89830586
Seymur Omarov <omarov.seymur@outlook.com>
2018-11-03 19:14:49 +0400

    license controller

---
+++ app/Http/Controllers/LicenseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\License;
+use Illuminate\Http\Request;
+
+class LicenseController extends Controller
+{
+    public function check(Request $request)
+    {
+
+        $license = License::where("code", $request->license)->where('status', 0)->get();
+
+        $count = is_array($license) ? count($license) : 0;
+
+        if ($count != 0) {
+            return response()->json([
+                "success" => 0
+            ]);
+        } else {
+            License::where("code", $request->license)->where('status', 0)->update([
+                "status" => 1
+            ]);
+            return response()->json([
+                "success" => 1
+            ]);
+        }
+    }
+}
